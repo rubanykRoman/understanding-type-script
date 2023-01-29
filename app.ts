7e6d7a28ca68f5cda612c1d9bb86130dc002e5e7
romanrubanyk <roman.rubanyk@onix-systems.com>
2023-01-29 05:43:34 +0200

    lesson 23-24

--- app.ts
+++ app.ts
@@ -1,15 +1,29 @@
-function combine (input1: number | string , input2: number | string ) {
-    let result;
-    if (typeof input1 ===  'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor,
+) {
+  let result;
+  if ((typeof input1 === 'number' && typeof input2 === 'number') || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // }
+  // return result.toString();
 }
 
-const combinedAges = combine (30,26)
-console.log(combinedAges)
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
 
-const combinedNames = combine ("Max","Anna")
-console.log(combinedNames)
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
