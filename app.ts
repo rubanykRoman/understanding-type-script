7873a5a46c527e2b46f94de3ed980d7ac90019e9
romanrubanyk <roman.rubanyk@onix-systems.com>
2023-01-29 02:41:40 +0200

    lesson 14

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("I am working");
+function add(n1: number, n2: number, showResult: boolean) {
+    if (showResult) {
+        console.log(n1 + n2);
+    }
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+
+add(number1, number2, printResult);
